48a437c1ba9455408893fc32768051a35823fc14
DefinitelyNotCam <DefinitelyNotCam>
2023-09-23 08:23:33 -0400

    Changes

--- src/components/CustomCursor.tsx
+++ src/components/CustomCursor.tsx
@@ -1,19 +1,19 @@
 import { useState } from "react";
 interface IMousePos {
-    left: number;
-    top: number;
+	left: number;
+	top: number;
 }
 
 export const CustomCursor = (): JSX.Element => {
-    const [mousePos, setMousePos] = useState<IMousePos>({ left: 0, top: 0 })
+	const [mousePos, setMousePos] = useState<IMousePos>({ left: 0, top: 0 })
 
-    document.addEventListener('mousemove', (e: MouseEvent) => {
-        setMousePos({ left: e.pageX, top: e.pageY });
-    })
+	document.addEventListener('mousemove', (e: MouseEvent) => {
+		setMousePos({ left: e.pageX, top: e.pageY });
+	})
 
-    return (
-            <div className='circle-follow no-cursor' 
-                id="circle-follow" 
-                style={mousePos}/>
-    )
+	return (
+		<div className='circle-follow no-cursor'
+			id="circle-follow"
+			style={mousePos} />
+	)
 }
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,24 +1,24 @@
 export const Hero = (): JSX.Element => {
-    return (
-        <div className="centered-container no-select">
-            <h1 className="header-name header-lg">
-                {'\u00A0'}
-                <span className="color-alt">CAM</span>ERON YOUNG
-                <span className="blinking-text">_</span>
-            </h1>
-            <h1 className="header-name header-md">
-                {'\u00A0'}
-                <span className="color-alt">CAM</span>ERON Y
-                <span className="blinking-text">_</span>
-            </h1>
-            <h1 className="header-name header-sm">
-                {'\u00A0'}
-                <span className="color-alt">CAM</span> Y
-                <span className="blinking-text">_</span>
-            </h1>
-            <p className="header-description">
-                 SOFTWARE, LEARNING, and <span className="color-alt">COFFEE</span>
-            </p>
-        </div>
-    )
+	return (
+		<div className="centered-container no-select">
+			<h1 className="header-name header-lg">
+				{'\u00A0'}
+				<span className="color-alt">CAM</span>ERON YOUNG
+				<span className="blinking-text">_</span>
+			</h1>
+			<h1 className="header-name header-md">
+				{'\u00A0'}
+				<span className="color-alt">CAM</span>ERON Y
+				<span className="blinking-text">_</span>
+			</h1>
+			<h1 className="header-name header-sm">
+				{'\u00A0'}
+				<span className="color-alt">CAM</span> Y
+				<span className="blinking-text">_</span>
+			</h1>
+			<p className="header-description">
+				SOFTWARE, LEARNING, and <span className="color-alt">COFFEE</span>
+			</p>
+		</div>
+	)
 }
