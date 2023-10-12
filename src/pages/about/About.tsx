b28ed567ed8f77277e1aeec47dee949061dd21f8
DefinitelyNotCam <DefinitelyNotCam>
2023-10-12 03:26:48 -0400

    begging about

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -1,9 +1,17 @@
 export default function About(): JSX.Element {
   return (
     <section id="about">
-      <h1> 
-        <span className="color-alt">Who</span> I am
-      </h1>
+      <div className="about">
+        <h1> 
+          <span className="color-alt">Who</span> I am
+        </h1>
+        <p>
+          My name is Cameron Young, and I am a software engineer.
+        </p> 
+        <p>
+          I am also a current student majoring in Information Technology with a focus on Software Engineering and minoring in Computer Science.
+        </p>
+      </div>
     </section>
   )
 }
