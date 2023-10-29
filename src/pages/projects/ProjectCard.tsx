3d5f3b31f0f8ba792f3697771e620a618957c3c6
ItsNotCam <camallanyoung@gmail.com>
2023-10-28 21:57:29 -0400

    made projects responsive

--- src/pages/projects/ProjectCard.tsx
+++ src/pages/projects/ProjectCard.tsx
@@ -1,8 +1,12 @@
 import { useState } from "react";
 import { CodeOutlined } from "@mui/icons-material";
 import { ProjectCardLinks } from "./ProjectCardLinks";
-import { IProject } from "./ProjectList";
+import { IProject, PROJECTS } from "./ProjectList";
 import { uuidv4 } from "../../utilities";
+
+import ArrowLeftOutlinedIcon from '@mui/icons-material/ArrowLeftOutlined';
+import ArrowDropDownOutlinedIcon from '@mui/icons-material/ArrowDropDownOutlined';
+import { IconButton } from '@mui/material';
 
 export interface IProjectCardProps {
   PROJECT: IProject;
@@ -22,6 +26,10 @@ export const styles = {
   }
 }
 
+const hasLinks = (PROJECT: IProject): boolean => {
+  return PROJECT.github_link !== undefined || PROJECT.demo_link !== undefined || PROJECT.readme_link !== undefined;
+}
+
 const ProjectTag = (props: { tag: string }): JSX.Element => {
   return (
     <span className="project-tag">{props.tag}</span>
@@ -30,6 +38,7 @@ const ProjectTag = (props: { tag: string }): JSX.Element => {
 
 export const ProjectCard = (props: IProjectCardProps): JSX.Element => {
   const [highlighted, setHighlighted] = useState<boolean>(false);
+  const [droppedDown, setDroppedDown] = useState<boolean>(false);
 
   const { PROJECT } = props;
   const mouseEvents: any = {
@@ -37,29 +46,96 @@ export const ProjectCard = (props: IProjectCardProps): JSX.Element => {
     onMouseLeave: () => setHighlighted(false)
   };
 
-  return (
-    <div className="project-card" {...mouseEvents}>
-      <div className="project-card-header">
-        <CodeOutlined className="color-light color-alt-hover" style={styles.iconStyle} />
-        <ProjectCardLinks 
-          demoLink={PROJECT.demo_link} 
-          readmeLink={PROJECT.readme_link} 
-          gitLink={PROJECT.github_link} 
-          setShowReadmeContent={props.setShowReadmeContent}
-          setReadmeContent={props.setReadmeContent}
-        />
-      </div>
-      {highlighted
+  const card_large: JSX.Element = (<>
+    <div className="project-card-header">
+      <CodeOutlined className="color-light color-alt-hover" style={styles.iconStyle} />
+      <ProjectCardLinks 
+        demoLink={PROJECT.demo_link} 
+        readmeLink={PROJECT.readme_link} 
+        gitLink={PROJECT.github_link} 
+        setShowReadmeContent={props.setShowReadmeContent}
+        setReadmeContent={props.setReadmeContent}
+      />
+    </div>
+    {highlighted
+      ? <h1 className="color-alt">{PROJECT.name}</h1>
+      : <h1 className="color-light">{PROJECT.name}</h1>}
+    <ul className="project-tags-lg">
+      {PROJECT.tags.map(tag => 
+        <li key={uuidv4()}>
+          <ProjectTag tag={tag}/>
+        </li>
+      )}
+    </ul>
+    {PROJECT.description.map(
+      description => <p key={uuidv4()}>{description}</p>
+    )}
+  </>)
+
+  const card_small: JSX.Element = (
+    <>
+      <div className="project-card-sm-info">
+        {droppedDown
         ? <h1 className="color-alt">{PROJECT.name}</h1>
         : <h1 className="color-light">{PROJECT.name}</h1>}
-      <ul className="project-tags">
-        {PROJECT.tags.map(tag => 
-          <li key={uuidv4()}>
-            <ProjectTag tag={tag}/>
-          </li>
-        )}
-      </ul>
-      <p>{PROJECT.description}</p>
+        <ul className="project-tags-sm">
+          {PROJECT.tags.map(tag => 
+            <li key={uuidv4()}>
+              <ProjectTag tag={tag}/>
+            </li>
+          )}
+        </ul>
+        {droppedDown
+          ? (<>
+            <div className="project-card-sm-desc">
+              {PROJECT.description.map(
+                description => <p key={uuidv4()}>{description}</p>
+              )}
+            </div>
+            {hasLinks(PROJECT)
+              ? <h2 className="color-alt">Links</h2>
+              : null}
+
+            <ProjectCardLinks 
+              demoLink={PROJECT.demo_link} 
+              readmeLink={PROJECT.readme_link} 
+              gitLink={PROJECT.github_link} 
+              setShowReadmeContent={props.setShowReadmeContent}
+              setReadmeContent={props.setReadmeContent}
+            />
+          </>)
+          : null
+        }
+      </div>
+      <div className="project-card-sm-dropdown">
+        <IconButton onClick={() => setDroppedDown(!droppedDown)}>
+          {droppedDown
+            ? (
+              <ArrowDropDownOutlinedIcon  
+                className="project-icon-button"
+                style={{
+                  fontSize: "2rem"
+                }}
+              />
+            ) : (
+              <ArrowLeftOutlinedIcon
+                className="project-icon-button"
+                style={{
+                  fontSize: "2rem"
+                }}
+              />
+            )}
+        </IconButton>
+      </div>
+    </>
+  )
+
+  return (<>
+    <div className="project-card-lg" {...mouseEvents}>
+      {card_large}
     </div>
-  );
+    <div className="project-card-sm" {...mouseEvents} onClick={() => setDroppedDown(!droppedDown)}>
+      {card_small}
+    </div>
+  </>);
 };
--- src/pages/projects/ProjectList.ts
+++ src/pages/projects/ProjectList.ts
@@ -2,7 +2,7 @@ export interface IProject {
   name: string,
   month: string,
   year: string,
-  description: string,
+  description: Array<string>,
   company_link?: ILink,
   github_link?: ILink,
   tags: Array<string>
@@ -20,20 +20,21 @@ export const PROJECTS: Array<IProject> = [
     name: "😊 This Website",
     month: "Oct",
     year: "2023",
-    description: `
-      This website was designed and written by me. It was developed using ReactJS with Typescript and 
+    description: [`
+      This website was designed and written by me.`,`It was developed using ReactJS with Typescript and 
       vanilla CSS (no CSS frameworks or html templates were used).
-    `,
+    `],
     tags: [ "ReactJS", "Typescript", "Vanilla CSS" ],
   },
   {
     name: "PCAP Extractor",
     month: "Dec",
     year: "2019",
-    description: `
-      Full stack web application developed for BAE Systems, Inc. It extracts information from packet capture files 
-      and stores them in a NoSQL database. Exposes an API for integration into other internal tools.
-    `,
+    description: [`
+      Full stack web application developed for BAE Systems, Inc.`, 
+      `It extracts information from packet capture files 
+      and stores them in a NoSQL database. It also exposes an API for integration into other internal tools.
+    `],
     company_link: {name: "BAE Systems, Inc.", link: "https://www.baesystems.com/en/home"},
     tags: [ "Python", "Javascript", "Flask", "ReactJS", "Docker" ]
   },
@@ -41,10 +42,10 @@ export const PROJECTS: Array<IProject> = [
     name: "Home File Server App",
     month: "January",
     year: "2020",
-    description: `
+    description: [`
       Containerized full stack web application that allows me to manipulate the file 
       system of my home server.
-    `,
+    `],
     tags: [ "Python", "Javascript", "Flask","ReactJS", "Docker" ],
     github_link: {
         name: "Repo", 
@@ -55,10 +56,10 @@ export const PROJECTS: Array<IProject> = [
     name: "VR Smart Watch",
     month: "June",
     year: "2022",
-    description: `
+    description: [`
       A virtual reality smart watch that connects the real world to the virtual world. 
       It displays in real time a user's local time and weather, their heart rate, and more.
-    `,
+    `],
     tags: [ "C#", "WebSockets", "Multiprocessing" ],
     github_link: {
       name: "Repo",
@@ -69,10 +70,11 @@ export const PROJECTS: Array<IProject> = [
     name: "\'Coordinates Book\' Minecraft Mod",
     month: "June",
     year: "2019",
-    description: `
-      Modification for the popular video game Minecraft. This automates the process of saving your favorite locations
+    description: [`
+      Modification for the popular video game Minecraft.`,
+      `This automates the process of saving your favorite locations
       within the game, replacing traditional pen-and-paper methods.
-    `,
+    `],
     tags: [ "Java", "NoSQL" ],
     github_link: {
       name: "Repo",
@@ -87,8 +89,8 @@ export const PROJECTS: Array<IProject> = [
     name: "HeaterPi",
     month: "September",
     year: "2023",
-    description: `Full stack web application that controls the temperature in a room using a Raspberry Pi and a standard space heater. Uses websockets for 
-    real-time communication between the client and the device.`,
+    description: [`Full stack web application that controls the temperature in a room using a Raspberry Pi and a standard space heater.`,`Uses websockets for 
+    real-time communication between the client and the device.`],
     tags: [ "Python", "Typescript", "ReactJS" ],
     readme_link: { name: "Readme", link: "https://raw.githubusercontent.com/ItsNotCam/HeaterPi-Server/master/README.md" },
     github_link:  { name: "Repo", link: "https://github.com/ItsNotCam/HeaterPi-Server" },
