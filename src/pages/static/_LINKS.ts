10801f9d98987c2c37c154eb2c4a6b3c94a750df
ItsNotCam <camallanyoung@gmail.com>
2024-01-05 12:53:20 -0500

    Updated README file name

--- src/pages/static/_LINKS.ts
+++ src/pages/static/_LINKS.ts
@@ -1,4 +1,4 @@
 export const LINKED_IN: string = "https://www.linkedin.com/in/cameron-young-37b173129/";
 export const GITHUB: string = "https://github.com/ItsNotCam";
 export const EMAIL: string = "mailto:cyoung35@gmu.edu";
-export const RESUME: string = "Cameron Young Resume.pdf";
+export const RESUME: string = "Cameron_Young_Resume.pdf";
